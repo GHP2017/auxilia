ca52be9757e70638d83292cd0e37eea28730df1e
Chase Williams <bornofawesomeness@gmail.com>
2017-07-01 14:20:44 +0000

    added mouse logic

--- lib/mouse.py
+++ lib/mouse.py
@@ -1,14 +1,26 @@
-
+import os
+import time
 class Mouse:
 
-    def __init(self):
+    def __init__(self):
         pass
 
     def playSong(track_id):
-        pass
+        os.system("xdotool mousemove 45 260")
+        time.sleep(.1)
+        os.system("xdotool click 1")
+        time.sleep(.2)
+        os.system("xdotool mousemove 45 200")
+        time.sleep(.1)
+        os.system("xdotool click 1")
+        time.sleep(.3)
+        os.system("xdotool type "+ track_id)
+        time.sleep(.2)
+        os.system("xdotool key KP_Enter")
 
     def pause():
-        pass
+        os.system("xdotool key KP_Space")
     
     def play():
-        pass
+        os.system("xdotool key KP_Space")
+    
