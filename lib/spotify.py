71be24b3f2d946a41a258d531d20da3ba17d29d0
chaseWilliams <bornofawesomeness@gmail.com>
2017-07-03 10:42:26 -0400

    refactor complete for spotify

--- lib/spotify.py
+++ lib/spotify.py
@@ -1,3 +1,46 @@
+import requests as http
+import redis as rd
+from lib.Song import Song
+
+track_uri = 'https://api.spotify.com/v1/tracks/'
+cache = rd.StrictRedis(host='localhost', port=6379, db=0)
+
+def get_request(url, call_type='GET', body=None):
+    access_token = cache.get('access_token').decode('utf-8')
+    if call_type is 'GET':
+        response = http.get(url, headers={'Authorization': 'Bearer ' + access_token})
+        if int(response.status_code) >= 400:
+            print(response.status_code)
+            refresh_access_token()
+            response = http.get(url, headers={'Authorization': 'Bearer ' + access_token})
+    if call_type is 'POST':
+        response = http.post(url, data=body, headers={'Authorization': 'Bearer ' + access_token})
+        print(response.text)
+        if int(response.status_code) >= 400:
+            refresh_access_token()
+            response = http.post(url, data=body, headers={'Authorization': 'Bearer ' + access_token})
+    if call_type is 'PUT':
+        response = http.put(url, data=body, headers={'Authorization': 'Bearer ' + access_token})
+        print(response.text, response.status_code)
+        if int(response.status_code) >= 400:
+            refresh_access_token()
+            response = http.put(url, data=body, headers={'Authorization': 'Bearer ' + access_token})
+
+    return response
+
+
+def refresh_access_token():
+    body = {
+        'grant_type': 'refresh_token',
+        'refresh_token': cache.get('refresh_token').decode('utf-8')
+    }
+    string = (client_id + ':' + client_secret).encode('utf-8')
+    encoded_string = str(b64encode(string))
+    response = http.post(token_uri, data=body, headers={'Authorization': 'Basic ' + encoded_string})
+    data = response.json()
+    
+    cache.set('access_token', data['access_token'])
+
 # takes either a track id or a track object as returned by the spotify api
 def create_song(track):
     if type(track) is str:
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, request
 from flask_socketio import SocketIO, emit
 from lib.Queue import Queue
 from lib.Song import Song
+from lib.spotify import get_request, create_song
 import requests as http
 import json
 import redis as rd
@@ -100,44 +101,6 @@ def queue_change():
 
 
 ## Helper Methods
-
-
-
-def get_request(url, call_type='GET', body=None):
-    access_token = cache.get('access_token').decode('utf-8')
-    if call_type is 'GET':
-        response = http.get(url, headers={'Authorization': 'Bearer ' + access_token})
-        if int(response.status_code) >= 400:
-            print(response.status_code)
-            refresh_access_token()
-            response = http.get(url, headers={'Authorization': 'Bearer ' + access_token})
-    if call_type is 'POST':
-        response = http.post(url, data=body, headers={'Authorization': 'Bearer ' + access_token})
-        print(response.text)
-        if int(response.status_code) >= 400:
-            refresh_access_token()
-            response = http.post(url, data=body, headers={'Authorization': 'Bearer ' + access_token})
-    if call_type is 'PUT':
-        response = http.put(url, data=body, headers={'Authorization': 'Bearer ' + access_token})
-        print(response.text, response.status_code)
-        if int(response.status_code) >= 400:
-            refresh_access_token()
-            response = http.put(url, data=body, headers={'Authorization': 'Bearer ' + access_token})
-
-    return response
-
-
-def refresh_access_token():
-    body = {
-        'grant_type': 'refresh_token',
-        'refresh_token': cache.get('refresh_token').decode('utf-8')
-    }
-    string = (client_id + ':' + client_secret).encode('utf-8')
-    encoded_string = str(b64encode(string))
-    response = http.post(token_uri, data=body, headers={'Authorization': 'Basic ' + encoded_string})
-    data = response.json()
-    
-    cache.set('access_token', data['access_token'])
 
 ## Testing only
 
