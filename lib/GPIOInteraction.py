1fec7adf50c9c307ca129b38ce7050ad1f7519f9
Chase Williams <bornofawesomeness@gmail.com>
2017-07-04 15:44:15 +0000

    Renamed functions to follow standards and fixed typos

--- lib/GPIOInteraction.py
+++ lib/GPIOInteraction.py
@@ -1,16 +1,18 @@
 import RPi.GPIO as GPIO
 import time
-
-def buttonPressed(channel):
-    print("pressed")
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-GPIO.add_event_detect(18, GPIO.RISING, callback=buttonPressed,bouncetime=300)
-
-while True:
-    time.sleep(1)
-    pass
-
-GPIO.cleanup()
+class GPIOInteractor:
+    
+    def buttonPressed(self, channel):
+        self.buttonCallback()
+        
+    def __init__(self):
+        self.buttonCallback = None
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(18, GPIO.RISING, callback=self.buttonPressed,bouncetime=300)
+        
+    def setButtonCallback(self, funct):
+        self.buttonCallback = funct
+        
+    def cleanup(self):
+        GPIO.cleanup()
--- playback_master.py
+++ playback_master.py
@@ -1,8 +1,25 @@
 import requests as http
 from lib.mouse import Mouse
+from lib.GPIOInteraction import GPIOInteractor
 import time
 
+# called whenever the physical pause/play button is pressed
+def toggle_play_pause():
+    if(playing):
+        m.pause()
+        playing = False
+        paused_at = time.time()
+    elif(not playing):
+        m.play()
+        playing = True
+        pause_time += time.time() - paused_at
+
+pause_time = 0
+paused_at = None
+playing = False
 m = Mouse()
+g = GPIOInteractor()
+g.setButtonCallback(toggle_play_pause)
 song_url = 'http://127.0.0.1:5000/get_next_song'
 response = http.get(song_url)
 
@@ -11,20 +28,25 @@ def get_next_song():
     # add error handling here
     return http.get(song_url).json()
 
+
+        
 # establish a connection
+"""
 while response.status_code != 200:
-    sleep(10)
+    time.sleep(10)
     response = http.get(song_url)
+"""
     
 data = response.json()
 duration = data['duration'] * 1000
 start_time = time.time()
 m.playSong(data['track_id'])
-
+playing = True
 # main loop
 while True:
-    if time.time() - start_time >= duration:
+    if time.time() - start_time - pause_time >= duration:
         data = get_next_song()
         duration = data['duration'] * 1000
         m.playSong(data['track_id'])
         start_time = time.time()
+        pause_time = 0
