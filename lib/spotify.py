96f5416ad0d98f12acc8b596a8efedf622faaa4a
Kai <kaimckeever@gmail.com>
2017-07-08 11:10:31 -0400

    wrote docstrings for spotify.py

--- lib/spotify.py
+++ lib/spotify.py
@@ -15,6 +15,7 @@ track_uri = 'https://api.spotify.com/v1/tracks/'
 recommendations_uri = 'https://api.spotify.com/v1/recommendations?seed_tracks='
 
 def get_request(url, call_type='GET', body=None):
+    """Returns response from server to GET, POST, or PUT requests"""
     access_token = cache.get('access_token').decode('utf-8')
     if call_type is 'GET':
         response = http.get(url, headers={'Authorization': 'Bearer ' + access_token})
@@ -41,6 +42,7 @@ def get_request(url, call_type='GET', body=None):
 
 
 def refresh_access_token():
+    """Refreshes access_token in cache."""
     body = {
         'grant_type': 'refresh_token',
         'refresh_token': cache.get('refresh_token').decode('utf-8')
@@ -56,6 +58,7 @@ def refresh_access_token():
 
 # takes either a track id or a track object as returned by the spotify api
 def create_song(track, explicit=True):
+    """Returns a song based on a track_id or track object from the Spotify API."""
     if type(track) is str:
         min_track_id = track[14:]
         response = get_request(track_uri + min_track_id)
@@ -71,6 +74,7 @@ def create_song(track, explicit=True):
     return Song(track_name, track_id, track_artists, album_uri, album_name, duration, explicit)
 
 def get_implicit_songs(seeds, num):
+    """Returns implicit songs by calling the Spotify Recommendations API"""
     songs = ','.join([song['track_id'] for song in seeds])
     url = recommendations_uri + songs + '&limit=' + str(num)
     response = get_request(url)
@@ -78,6 +82,7 @@ def get_implicit_songs(seeds, num):
     return [create_song(track_obj, explicit=False).to_dict() for track_obj in data['tracks']]
 
 def get_medians(seeds):
+    """An (unused) method Kai wrote because he thought we needed it. Returns median valence and energy of seeds."""
     median_valence = statistics.median(song['valence'] for song in seeds)
     median_energy = statistics.median(song['energy'] for song in seeds)
     response = '&target_valence=' + median_valence + '&target_energy' + median_energy
